46759eaa2e9e6db0cfe1afd0642a34eb8cac2c62
gniewunio <gniewunio@o2.pl>
2018-02-25 16:53:08 +0100

    003 - equality in hibernate

--- src/main/java/eu/gniewko/spring5webapp/model/Author.java
+++ src/main/java/eu/gniewko/spring5webapp/model/Author.java
@@ -60,4 +60,31 @@ public class Author {
     public void setBooks(Set<Book> books) {
         this.books = books;
     }
+
+	@Override
+	public int hashCode() {
+		return (id == null) ? 0 : id.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", books=" + books + "]";
+	}
 }
--- src/main/java/eu/gniewko/spring5webapp/model/Book.java
+++ src/main/java/eu/gniewko/spring5webapp/model/Book.java
@@ -60,4 +60,32 @@ public class Book {
     public void setPublisher(String publisher) {
         this.publisher = publisher;
     }
+
+	@Override
+	public int hashCode() {
+		return(id == null) ? 0 : id.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", isbn=" + isbn + ", publisher=" + publisher + ", authors="
+				+ authors + "]";
+	}
 }
